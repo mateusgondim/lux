48b7ae26fc2b8834e1e3588ba6170ba63688f799
Mateus Gondim <mateusgondimlima@gmail.com>
2021-04-03 13:56:58 -0300

    Refactor Scene to handle the shapes
    Refactor the Scene class to both handle the shapes and perform the
    intersection against them.

--- src/core/scene.h
+++ src/core/scene.h
@@ -4,16 +4,29 @@
 #include <vector>
 #include <memory>
 
-namespace lux { class Shape; }
+namespace lux { class Ray; struct Surface_interaction; class Shape; }
 
 namespace lux {
-  class Scene {
+  class Scene final {
     public:
-      bool intersect;
-      bool intersect_p;
+      Scene() = default;
+      Scene(const Scene &) = delete;
+
+      ~Scene() = default;
+
+      Scene & operator=(const Scene &) = delete;
+
+      void add_shape(std::shared_ptr<Shape> pshape);
+
+      const std::vector<std::shared_ptr<Shape>> & get_shapes() const { return m_shapes; }
+      const std::vector<std::shared_ptr<Shape>> & get_lights() const { return m_lights; }
+
+      bool intersect(const Ray & ray, Surface_interaction * psurface_interaction) const;
+      bool intersect_p(const Ray & ray) const;
 
     private:
-      std::vector<std::shared_ptr<Shape>> m_pshapes;
+      std::vector<std::shared_ptr<Shape>> m_shapes;
+      std::vector<std::shared_ptr<Shape>> m_lights;
   };
 }
 
